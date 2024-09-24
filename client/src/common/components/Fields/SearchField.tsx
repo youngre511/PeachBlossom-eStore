eaa9f654cbccf8573740e807be767ac033ec06fa
Ryan Young <youngre511@gmail.com>
2024-09-24 14:44:43 -0400

    fix: fix search-field clear not working

--- client/src/common/components/Fields/SearchField.tsx
+++ client/src/common/components/Fields/SearchField.tsx
@@ -9,51 +9,6 @@ import React from "react";
 import { ChangeEvent, FormEvent, useState, ComponentProps } from "react";
 import SearchSharpIcon from "@mui/icons-material/SearchSharp";
 
-// interface Props {
-//     updateSearchParams: (newFilters: Record<string, string>) => void;
-//     sx?: ComponentProps<typeof TextField>["sx"];
-//     inputSx?: ComponentProps<typeof TextField>["sx"];
-// }
-// const SearchField: React.FC<Props> = ({ updateSearchParams, sx, inputSx }) => {
-//     const [searchQuery, setSearchQuery] = useState<string>("");
-
-//     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-//         e.preventDefault();
-//         updateSearchParams({ search: searchQuery });
-//     };
-//     return (
-//         <React.Fragment>
-//             <form onSubmit={handleSubmit}>
-//                 <TextField
-//                     label="Search"
-//                     variant="outlined"
-//                     value={searchQuery}
-//                     onChange={(e: ChangeEvent<HTMLInputElement>) =>
-//                         setSearchQuery(e.target.value)
-//                     }
-//                     fullWidth
-//                     InputProps={{
-//                         startAdornment: (
-//                             <InputAdornment position="start">
-//                                 <SvgIcon>
-//                                     <SearchSharpIcon />
-//                                 </SvgIcon>
-//                             </InputAdornment>
-//                         ),
-//                     }}
-//                     sx={sx || undefined}
-//                     inputProps={{
-//                         sx: inputSx ? inputSx : undefined,
-//                         inputMode: "search",
-//                     }}
-//                 />
-//                 <Button type="submit" style={{ display: "none" }} />
-//             </form>
-//         </React.Fragment>
-//     );
-// };
-// export default SearchField;
-
 interface Props {
     updateSearchParams: (newFilters: Record<string, string>) => void;
     sx?: ComponentProps<typeof TextField>["sx"];
@@ -78,7 +33,16 @@ const SearchField: React.FC<Props> = ({
                 <Autocomplete
                     freeSolo
                     id="search-bar"
-                    disableClearable
+                    onInputChange={(
+                        e: React.SyntheticEvent,
+                        value: string,
+                        reason: string
+                    ) => {
+                        if (reason === "clear") {
+                            setSearchQuery("");
+                            updateSearchParams({ search: "" });
+                        }
+                    }}
                     filterOptions={(options) => {
                         const inputValue = searchQuery.toLowerCase();
                         return searchQuery.length >= 2
